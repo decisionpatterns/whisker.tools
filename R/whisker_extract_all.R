029c24eb476316e5431e9ff04a4409503bc091d4
Christopher Brown <cbrown@decisionpatterns.com>
2014-10-07 15:23:03 -0700

    Forked from dp.misc library

---
+++ R/whisker_extract_all.R
@@ -0,0 +1,35 @@
+#' whisker_extract_all 
+#' 
+#'  Extract varaibles from a whisker template
+#'  
+#'  @param template character of template text
+#'  
+#'  Provides a stringr type function for extracting the names for the 
+#'  substituted variable. This is useful if you want to do some checking or 
+#'  other manipulation of the template prior to rendering.
+#'  
+#'  @return
+#'    chracter vector of names found in the template
+#'
+#'  @seealso 
+#'    \code{\link[whisker]{whisker.render}}
+#'    \code{\link[stringr]{str_extract_all}} on which this function relies  
+#'  @examples
+#'    template <- "Dear {{name}}, I have not loved you since {{date}}"
+#'    whisker_extract_all(template)
+#'  str_extract_all( template, pattern=perl("\\{\\{(.*)\\}\\}") )
+#'   
+
+whisker_extract_all <- function(template) {
+  
+  require(stringr)
+  
+  message( class(template) )
+  variables <- 
+    str_extract_all( template, pattern=perl( '{{(.+?)}}' ) )
+  variables <- lapply( variables, str_replace_all, pattern=perl( '[{}]'), replacement='')
+  # variables <- str_replace( variables, pattern=perl( '[{}]'), replacement='')
+  
+  unlist(variables)
+}
+  
---
+++ R/whisker_mapply.R
@@ -0,0 +1,54 @@
+#' whisker_mapply - repeatedly ender a template over multiple arguments 
+#' 
+#' Have a mixture of changing and unchanging template elements.
+#' 
+#' @param template character
+#' @param data named list or environment with variables that will be used 
+#' during rendering but do not change in each 
+#' @param partials See \code{\link[whisker]{whisker.render}}
+#' @param ... named list of object that get substituted 
+#' 
+#' \code{whisker_mapply} supports a mapply type interface over \code{whisker.render}
+#' that allows for vector of template renderings. 
+#' 
+#' The advantage of this approach are several-fold:  
+#' - more reusablity of the template
+#' - cleaner syntax
+#' - no need to be changing out data
+#' - less looping 
+#' - template rendered as a vector
+#' 
+#' @return 
+#'   character vector of rendered templates
+#'   
+#' @seealso 
+#'   \code{\link{whisker.render}}
+#'      
+#' @examples   
+#'   # template <- 'type {{type}}: {{uppercase}},{{lowercase}}'
+#'   # whisker_mapply( template, list(type="alphabet"), lowercase=letters[1:3], uppercase=LETTERS[1:3] )
+#'   
+#'   # USED IN LOOP ... 
+#'   #  template is pre-defined at top of code (above) and is not changes, 
+#'   #   nor is it required to modify data. This is handled seemlessly. 
+#'   \dontrun{
+#'   # for( type in 1:2 ) { 
+#'   #   print( whisker_mapply( template, list(type=type, uppercase="A"), lowercase=letters[1:3] ) )
+#'   # }
+#'   # }
+#'      
+#' @export   
+ 
+whisker_mapply <- function( template, data = parent.frame(), partials = list(), ... ) { 
+  
+  data <- as.list(data)
+  dots <- data.frame( list( ...) )
+
+  ret <- character()
+  for( row in 1:nrow(dots) ) {
+    newdata <- c( as.list( dots[row, , drop=FALSE] ), data  )
+    ret[[ length(ret)+1 ]] <- whisker.render( template=template, data=newdata, partials=partials ) 
+  }  
+  
+  return(ret)
+}
