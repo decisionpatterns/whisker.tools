f174bc9f3c6687eb49cd3b3e7070f60e6dae6493
Christopher Brown <cbrown@decisionpatterns.com>
2014-12-15 02:56:28 -0800

    Version 0.4.1
    + whisker_get_all now passed ... to get

--- R/whisker_get_all.R
+++ R/whisker_get_all.R
@@ -5,14 +5,16 @@
 #' 
 #' @param template character; a whisker template
 #' 
+#' @param ... additional arguments passed to \code{\link[base]{get}}
+#' 
 #' \code{whisker_get_all} searches the call sd
 #' 
 #' @return 
 #'   list containing the names and values from the 
 #'   
 #' @seealso 
+#'   \code{\link[base]{get}} \cr
 #'   \code{\link{whisker_names}} \cr
-#'   \code{\link[base]{get}}
 #'   
 #' @examples 
 #' 
@@ -28,7 +30,7 @@
 #' @export 
 
 
-whisker_get_all <- function(template) { 
+whisker_get_all <- function(template, ...) { 
 
   nms <- whisker_names(template)
 
@@ -38,11 +40,12 @@ whisker_get_all <- function(template) {
     value = try( get( nm ) )
  
     if( ! class(value) == 'try-error' ) 
-      li[[nm]] <- get(nm, envir = parent.frame() ) else 
+      li[[nm]] <- get(nm, ... ) else 
       warning( nm, " not found" )
   } 
   
   return(li)
+  
 }
 
 # whisker_get_all <- function( template ) { 
